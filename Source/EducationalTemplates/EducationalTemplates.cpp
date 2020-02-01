a541f6dba02bc9a8d8d3df7668b4253a6961df13
Cory Brown <17816149+CoryBrown-42@users.noreply.github.com>
2020-01-31 21:33:22 -0500

    Added C++ Class

---
+++ Source/EducationalTemplates/EducationalTemplates.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "EducationalTemplates.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, EducationalTemplates, "EducationalTemplates" );
